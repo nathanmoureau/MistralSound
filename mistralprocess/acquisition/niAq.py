93958825f417b975c9c55f0fa8b900c5ef32597a
Nathan Moureau <nathanmoureau@gmail.com>
2025-06-24 14:35:42 +0200

    acquisition via carte NI et premiere interaction live langmuir

---
+++ mistralprocess/acquisition/niAq.py
@@ -0,0 +1,14 @@
+import nidaqmx
+from nidaqmx.constants import AcquisitionType, READ_ALL_AVAILABLE
+
+
+def configAq(deviceName, fe, bsize, sample_mode=AcquisitionType.FINITE):
+    task = nidaqmx.Task()
+    task.ai_channels.add_ai_voltage_chan(deviceName)
+    task.timing.cfg_samp_clk_timing(
+        fe, sample_mode=sample_mode, samps_per_chan=bsize)
+    return task
+
+
+def getBuffer(task):
+    return task.read(READ_ALL_AVAILABLE)
---
+++ mistralprocess/main.py
@@ -0,0 +1,34 @@
+from sync.sendData import *
+from process.langmuir import *
+from acquisition.niAq import *
+import os
+import sys
+import time
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+MAIN_DIR = os.path.split(SCRIPT_DIR)[0]
+
+sys.path.append(os.path.dirname(MAIN_DIR + "/"))
+
+
+fs = 100000
+bsize = 1024
+lgmr_task = configAq("Dev1/ai0", fs, bsize)
+
+buffer = np.zeros(bsize)
+f0 = 0
+snr = 0
+phShift = 0
+
+oscSender = udp_client.UDPClient("localhost", 57210)
+
+print("Ready.")
+
+while True:
+    buffer[:] = getBuffer(lgmr_task)
+    f0 = get_f0(buffer, fs, bsize) / 10000
+    snr = get_noiseRatio(buffer)
+    phShift = get_phaseShift(buffer, buffer, fs, bsize)
+    sendMsg(oscSender, "/lgmr_freq", f0)
+    sendMsg(oscSender, "/lgmr_noise", f0)
+    sendMsg(oscSender, "/lgmr_phase", f0)
